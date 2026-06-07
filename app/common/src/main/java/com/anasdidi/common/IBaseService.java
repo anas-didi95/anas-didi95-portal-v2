cb7099160b7f735540f3598aa38ebee715741627
Anas Juwaidi <anas.didi95@tutamail.com>
2026-06-07 10:18:20 +0000

    refactor: replace @ExecuteTrace annotation with execution() pointcut on IBaseService
    - Change ExecuteTraceAspect from @annotation() to execution() pointcut
      targeting IBaseService.execute() directly, making it work with CGLIB proxies
    - Delete ExecuteTrace.java marker annotation (no longer needed)
    - Remove @ExecuteTrace import and usage from IBaseService and HelloWorldService
    - Extract named @Pointcut for maintainability
    - Guard args toString with isInfoEnabled() to avoid eager string allocation

--- app/common/src/main/java/com/anasdidi/common/IBaseService.java
+++ app/common/src/main/java/com/anasdidi/common/IBaseService.java
@@ -1,6 +1,5 @@
 package com.anasdidi.common;
 
-import com.anasdidi.common.aspect.ExecuteTrace;
 import jakarta.validation.Valid;
 import org.springframework.validation.annotation.Validated;
 import reactor.core.publisher.Mono;
@@ -8,6 +7,5 @@ import reactor.core.publisher.Mono;
 @Validated
 public interface IBaseService<A extends IBaseReqDTO, B extends IBaseResDTO> {
 
-  @ExecuteTrace
   Mono<B> execute(@Valid A req);
 }
--- app/common/src/main/java/com/anasdidi/common/aspect/ExecuteTraceAspect.java
+++ app/common/src/main/java/com/anasdidi/common/aspect/ExecuteTraceAspect.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
@@ -13,19 +14,26 @@ import reactor.core.publisher.Mono;
 @Component
 public class ExecuteTraceAspect {
 
-  @Around("@annotation(com.anasdidi.common.aspect.ExecuteTrace)")
+  @Pointcut("execution(* com.anasdidi.common.IBaseService.execute(..))")
+  void serviceExecution() {}
+
+  @Around("serviceExecution()")
   public Object traceExecution(ProceedingJoinPoint joinPoint) throws Throwable {
     String signature = joinPoint.getSignature().toShortString();
-    log.info("[{}] Request: {}", signature, Arrays.toString(joinPoint.getArgs()));
+
+    if (log.isInfoEnabled()) {
+      log.info("[{}] Request: {}", signature, Arrays.toString(joinPoint.getArgs()));
+    }
 
     Object result = joinPoint.proceed();
 
     if (!(result instanceof Mono<?> mono)) {
-      log.warn("[{}] @ExecuteTrace applied to non-Mono method, skipping response log", signature);
+      log.warn("[{}] returned non-Mono, skipping response log", signature);
       return result;
     }
 
-    return mono.doOnSuccess(res -> log.info("[{}] Response: {}", signature, res))
+    mono = mono.doOnSuccess(res -> log.info("[{}] Response: {}", signature, res))
         .doOnError(err -> log.error("[{}] Execution error", signature, err));
+    return mono;
   }
 }
